cf86fc0a82cf100fd8b1fc58c7fe3f2bd4929b9a
Denis Neves <nevesdenis35@gmail.com>
2025-04-03 09:38:52 -0300

    altera db e adiciona cors

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import express from "express";
+import cors from "cors";
+
 import { routes } from "./src/routes/routes.js";
 
 const app = express();
-const port = 3333;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors);
 
 routes.forEach(({ method, path, handler }) => {
   app[method](path, handler);
--- src/db.js
+++ src/db.js
@@ -1,16 +1,16 @@
 import mysql from "mysql2";
 
 export const connectionDb = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "escola-api",
+  host: "db4free.net",
+  user: "aidj1123",
+  password: "ryQ_QXhb9K5@2M@",
+  database: "tec_escola_api",
 });
 
 connectionDb.connect((erro) => {
   if (erro) {
     console.error(`Erro ao conectar: ${erro.message}`);
   } else {
+    console.log(`DB Conectado em: ${connectionDb.config.host}`);
   }
-  console.log(`DB Conectado com sucesso`);
 });
